1632d145bad2ccb3c63b97247055d8961d26d69e
alvido <64369060+alvido@users.noreply.github.com>
2023-12-09 18:21:26 +0200

    form logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,9 +140,9 @@ function formatInput(input) {
   // Проверка ввода по регулярному выражению
   var pattern = /^[A-Za-z]{2}-\d{6}$/;
   if (!pattern.test(input.value)) {
-      input.classList.add('error');
+    input.classList.add('error');
   } else {
-      input.classList.remove('error');
+    input.classList.remove('error');
   }
 }
 //
@@ -221,94 +221,153 @@ function toggleClasses(button) {
 
 
 // upload files drag and drop
-const dropFileZone = document.getElementById("inputWrapper")
-const statusText = document.getElementById("uploadForm_Status")
-const sizeText = document.getElementById("uploadForm_Size")
-const uploadInput = document.getElementById("fileInput")
+const dropFileZone = document.getElementById("inputWrapper");
+const statusText = document.getElementById("uploadForm_Status");
+const sizeText = document.getElementById("uploadForm_Size");
+const uploadInput = document.getElementById("fileInput");
+const itemList = document.getElementById("itemList");
+const formDropContainer = document.querySelector('.information__form--step-drop-container'); // Новая строка
+
+let selectedFiles = [];
 
 let setStatus = (text) => {
-  statusText.textContent = text
-}
+  statusText.textContent = text;
+};
 
 const uploadUrl = "/unicorns";
 
-["dragover", "drop"].forEach(function(event) {
-  document.addEventListener(event, function(evt) {
-    evt.preventDefault()
-    return false
-  })
-})
+["dragover", "drop"].forEach(function (event) {
+  document.addEventListener(event, function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+});
 
-dropFileZone.addEventListener("dragenter", function() {
-  dropFileZone.classList.add("_active")
-})
+dropFileZone.addEventListener("dragenter", function () {
+  dropFileZone.classList.add("_active");
+});
 
-dropFileZone.addEventListener("dragleave", function() {
-  dropFileZone.classList.remove("_active")
-})
+dropFileZone.addEventListener("dragleave", function () {
+  dropFileZone.classList.remove("_active");
+});
 
-dropFileZone.addEventListener("drop", function() {
-  dropFileZone.classList.remove("_active")
-  const file = event.dataTransfer?.files[0]
-  if (!file) {
-    return
-  }
+dropFileZone.addEventListener("drop", function () {
+  dropFileZone.classList.remove("_active");
 
-  if (file.type.startsWith("image/")) {
-    uploadInput.files = event.dataTransfer.files
-    processingUploadFile()
-  } else {
-    setStatus("Kun billeder kan uploades")
-    return false
-  }
-})
+  const newFiles = Array.from(event.dataTransfer?.files);
+
+  selectedFiles = selectedFiles.concat(newFiles);
+
+  updateFileList();
+
+  // Вызываем функцию для добавления/удаления класса
+  addImagesClass();
+});
 
 uploadInput.addEventListener("change", (event) => {
-  const file = uploadInput.files?.[0]
-  if (file && file.type.startsWith("image/")) {
-    processingUploadFile()
-  } else {
-    setStatus("Kun billeder kan uploades")
-    return false
-  }
-})
+  const newFiles = Array.from(uploadInput.files);
+
+  selectedFiles = selectedFiles.concat(newFiles);
+
+  updateFileList();
+
+  // Вызываем функцию для добавления/удаления класса
+  addImagesClass();
+});
 
 function processingUploadFile(file) {
   if (file) {
-    const dropZoneData = new FormData()
-    const xhr = new XMLHttpRequest()
+    const dropZoneData = new FormData();
+    const xhr = new XMLHttpRequest();
 
-    dropZoneData.append("file", file)
+    dropZoneData.append("file", file);
 
-    xhr.open("POST", uploadUrl, true)
+    xhr.open("POST", uploadUrl, true);
 
-    xhr.send(dropZoneData)
+    xhr.send(dropZoneData);
 
     xhr.onload = function () {
       if (xhr.status == 200) {
-        setStatus("All upload")
+        setStatus("All upload");
+
+        updateFileList();
+
+        // Вызываем функцию для добавления/удаления класса
+        addImagesClass();
       } else {
-        setStatus("Error")
+        setStatus("Error");
       }
-      HTMLElement.style.display = "none"
-    }
+      HTMLElement.style.display = "none";
+    };
   }
 }
 
-function processingDownloadFileWithFetch() {
-  fetch(url, {
-    method: "POST",
-  }).then(async (res) => {
-    const reader = res?.body?.getReader();
-    while (true && reader) {
-      const { value, done } = await reader?.read()
-      console.log("value", value)
-      if (done) break
-      console.log("Received", value)
-    }
-  })
+function updateFileList() {
+  itemList.innerHTML = "";
+
+  selectedFiles.forEach((file, index) => {
+    const listItem = document.createElement("li");
+
+    const spanElement = document.createElement("span");
+    spanElement.textContent = `${file.name} (${(file.size / (1024 * 1024)).toFixed(2)} MB)`;
+    
+    listItem.appendChild(spanElement);
+
+
+    // Создаем новый элемент svg для иконки "icon-check"
+    const checkIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    checkIcon.setAttribute("class", "icon icon-check");
+    checkIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    checkIcon.setAttribute("height", "16");
+    checkIcon.setAttribute("width", "14");
+    checkIcon.setAttribute("viewBox", "0 0 448 512");
+
+    // Создаем новый элемент path для иконки "icon-check"
+    const checkPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    checkPath.setAttribute("d", "M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z");
+    
+    // Добавляем path в svg
+    checkIcon.appendChild(checkPath);
+
+    // Добавляем svg в span
+    spanElement.prepend(checkIcon);
+
+    // Создаем новый элемент svg для иконки "icon-remove"
+    const removeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    removeIcon.setAttribute("class", "icon icon-remove");
+    removeIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    removeIcon.setAttribute("height", "16");
+    removeIcon.setAttribute("width", "14");
+    removeIcon.setAttribute("viewBox", "0 0 448 512");
+
+    const removePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    removePath.setAttribute("d", "M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z");
+
+    removeIcon.appendChild(removePath);
+
+    removeIcon.addEventListener("click", function () {
+      selectedFiles.splice(index, 1);
+
+      updateFileList();
+
+      // Вызываем функцию для добавления/удаления класса
+      addImagesClass();
+    });
+
+    listItem.appendChild(removeIcon);
+
+    itemList.appendChild(listItem);
+  });
 }
 
-
+// Функция для добавления/удаления класса
+function addImagesClass() {
+  if (selectedFiles.length > 0) {
+    formDropContainer.classList.add('has-images');
+  } else {
+    formDropContainer.classList.remove('has-images');
+  }
+}
+//
 
 
